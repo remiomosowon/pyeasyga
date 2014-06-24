3ceea538142b4594ea8523f271b7023559a292f2
Ayodeji Remi-Omosowon <ayodeji.remi-omosowon2008@my.ntu.ac.uk>
2014-06-24 11:01:48 +0100

    Various housekeeping tasks
    * Implemented an alternate way of getting the package version in setup.py due
    to issues with importing the package into setup.py (see issue and solution at
    https://github.com/jeffknupp/sandman/issues/11).
    
    * Changed documentation html_theme from 'default' to 'pyramid'.
    
    * Modified .travis.yml install script to use pip mirrors (i.e. --use-mirrors).
    
    * Added 'clean-tox' target to Makefile to clean the *.egg files generated by
    tox.
    
    * Moved development requirements to requirements.dev (from requirements.txt).
    The idea is to keep only actual package requirements in requirements.txt but
    still maintain the development requirements.
    
    * Removed support for py33 and py34 environments. The plan is to initially
    support only py2.x environments and then later provide suport for py3.x.

--- setup.py
+++ setup.py
@@ -3,27 +3,40 @@
 
 import os
 import sys
-
-import pyeasyga
+import re
+import codecs
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    # intentionally *not* adding an encoding option to open
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                                version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+        raise RuntimeError("Unable to find version string.")
+
 
 readme = open('README.rst').read()
 history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 requirements = [
-    # TODO: put package requirements here
 ]
 
 test_requirements = ['tox']
 
 setup(
     name='pyeasyga',
-    version=pyeasyga.__version__,
+    version=find_version('pyeasyga', '__init__.py'),
     description='An easy-to-use Genetic Algorithm implementation in Python',
     long_description=readme + '\n\n' + history,
     author='Ayodeji Remi-Omosowon',
@@ -47,9 +60,6 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
     ],
     test_suite='tests',
     tests_require=test_requirements
