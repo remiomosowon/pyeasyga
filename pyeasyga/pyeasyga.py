bc30df1cac8047371893aae5c4bfb11961763352
Ayodeji Remi-Omosowon <remiomosowon@gmail.com>
2014-06-29 18:39:33 +0100

    Completed GA functionality and tests
    Completed all basic GA funcationality, and implemented basic tests to cover all
    code.

--- pyeasyga/pyeasyga.py
+++ pyeasyga/pyeasyga.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import random
+import copy
 from operator import attrgetter
 
 
@@ -7,70 +9,130 @@ class GeneticAlgorithm(object):
                  seed_data,
                  population_size=100,
                  generations=300,
-                 elitism=True,
                  crossover_probability=0.8,
                  mutation_probability=0.2):
         self.seed_data = seed_data
         self.population_size = population_size
         self.generations = generations
-        self.elitism = elitism
         self.crossover_probability = crossover_probability
         self.mutation_probability = mutation_probability
-        self.current_generation = []
 
+        self.current_generation = []
+        self.elitism = True
+        self.maximise_fitness = True
+        self.create_individual = None
         self.fitness_function = None
         self.crossover_function = None
         self.mutate_function = None
         self.selection_function = None
-        self.create_individual = None
 
-    def create_initial_population(self, population_size, create_individual):
+    def create_initial_population(
+            self, seed_data, population_size, create_individual):
         initial_population = []
-        for individual_id in xrange(population_size):
-            genes = create_individual()
-            individual = Chromosome(individual_id, genes)
+        for _ in xrange(population_size):
+            genes = create_individual(seed_data)
+            individual = Chromosome(genes)
             initial_population.append(individual)
         return initial_population
 
-    def calculate_population_fitness(self, population, fitness_function):
+    def calculate_population_fitness(self, data, population, fitness_function):
         for individual in population:
-            individual.fitness = fitness_function(individual.genes)
+            individual.fitness = fitness_function(individual.genes, data)
 
     def rank_population(self, population, maximise_fitness=True):
-        self.population.sort(key=attrgetter('fitness'),
-                             reversed=maximise_fitness)
+        population.sort(key=attrgetter('fitness'), reverse=maximise_fitness)
 
-    def create_new_population(self, population, crossover, mutate, selection):
-        pass
+    def create_new_population(
+            self, population, crossover, prob_crossover, mutate, prob_mutate,
+            selection, elitism):
+        new_population = []
+        best = copy.deepcopy(population[0])
 
-    def create_first_generation(self, population_size, create_individual,
-                                fitness_function, maximise_fitness):
-        initial_population = self.create_initial_population(population_size,
-                                                            create_individual)
-        self.calculate_population_fitness(initial_population, fitness_function)
+        while len(new_population) < len(population):
+            parent_1 = copy.deepcopy(selection(population))
+            parent_2 = copy.deepcopy(selection(population))
+
+            can_crossover = random.random() < prob_crossover
+            can_mutate = random.random() < prob_mutate
+
+            child_1, child_2 = parent_1, parent_2
+            child_1.fitness, child_2.fitness = 0, 0
+
+            if can_crossover:
+                child_1.genes, child_2.genes = crossover(parent_1.genes,
+                                                         parent_2.genes)
+
+            if can_mutate:
+                mutate(child_1.genes)
+                mutate(child_2.genes)
+
+            new_population.append(child_1)
+            new_population.append(child_2)
+
+        if elitism:
+            new_population[0] = best
+
+        return new_population
+
+    def create_first_generation(
+            self, data, population_size, create_individual, fitness_function,
+            maximise_fitness=True):
+        initial_population = self.create_initial_population(
+            data, population_size, create_individual)
+        self.calculate_population_fitness(
+            data, initial_population, fitness_function)
         self.rank_population(initial_population, maximise_fitness)
         return initial_population
 
-    def create_next_generation(self, population, fitness_function,
-                               crossover_function, mutate_function,
-                               selection_function, maximise_fitness):
-        new_population = self.create_new_population(population,
-                                                    crossover_function,
-                                                    mutate_function,
-                                                    selection_function)
-        self.calculate_population_fitness(new_population, fitness_function)
+    def create_next_generation(
+            self, data, population, fitness_function,
+            selection_function, crossover_function, prob_crossover,
+            mutate_function, prob_mutate, elitism, maximise_fitness=True):
+        new_population = self.create_new_population(
+            population, crossover_function, prob_crossover, mutate_function,
+            prob_mutate, selection_function, elitism)
+        self.calculate_population_fitness(
+            data, new_population, fitness_function)
         self.rank_population(new_population, maximise_fitness)
         return new_population
 
-    def best_individual(self):
-        pass
+    def run(self):
+        data = self.seed_data
+        pop_size = self.population_size
+        generations = self.generations
+        create_func = self.create_individual
+        fitness_func = self.fitness_function
+        selection = self.selection_function
+        crossover = self.crossover_function
+        prob_crossover = self.crossover_probability
+        mutate = self.mutate_function
+        prob_mutate = self.mutation_probability
+        elitism = self.elitism
+        max_fitness = self.maximise_fitness
+
+        self.current_generation = self.create_first_generation(
+            data, pop_size, create_func, fitness_func, max_fitness)
+
+        for generation in xrange(1, generations):
+            next_generation = self.create_next_generation(
+                data, self.current_generation, fitness_func, selection,
+                crossover, prob_crossover, mutate, prob_mutate, elitism,
+                max_fitness)
+            self.current_generation = next_generation
+
+    def best_individual(self, population):
+        best = population[0]
+        return (best.fitness, best.genes)
 
     def last_generation(self):
-        pass
+        return ((member.fitness, member.genes) for member
+                in self.current_generation)
 
 
 class Chromosome(object):
-    def __init__(self, chromosome_id, genes):
-        self.chromosome_id = chromosome_id
+    def __init__(self, genes):
         self.genes = genes
-        self.fitness = 0.0
+        self.fitness = 0
+
+    def __repr__(self):
+        return repr((self.fitness, self.genes))
--- tests/test_pyeasyga.py
+++ tests/test_pyeasyga.py
@@ -10,23 +10,25 @@ Tests for `pyeasyga` module.
 
 import unittest
 import random
+import copy
+from operator import attrgetter
 
 from pyeasyga import pyeasyga
 
 
 class TestPyeasyga(unittest.TestCase):
 
-    ''' Test Problem: Assume you can only carry two items to sell in your bag.
+    ''' Test Problem: Assume you can only carry three items to sell in your bag.
         Pick a combination of items that maximises your profit. The profit for
         each item is listed. '''
     def setUp(self):
         self.seed_data = [('apple', 15), ('banana', 10), ('carrot', 12),
-                          ('pear', 5)]
+                          ('pear', 5), ('mango', 8)]
 
-        member_1 = [0, 1, 0, 1]
-        member_2 = [1, 1, 0, 0]
-        member_3 = [1, 1, 1, 1]
-        member_4 = [0, 1, 1, 1]
+        member_1 = [0, 1, 0, 1, 1]
+        member_2 = [1, 1, 0, 0, 1]
+        member_3 = [1, 1, 1, 1, 0]
+        member_4 = [0, 1, 1, 1, 1]
 
         self.population = []
         self.population.append(member_1)
@@ -34,103 +36,284 @@ class TestPyeasyga(unittest.TestCase):
         self.population.append(member_3)
         self.population.append(member_4)
 
-    def test_genetic_algorithm_initialisation_1(self):
-        ga = pyeasyga.GeneticAlgorithm(self.seed_data)
-        assert ga.population_size == 100
-        assert ga.generations == 300
-        assert ga.elitism is True
-        assert ga.crossover_probability == 0.8
-        assert ga.mutation_probability == 0.2
-
-    def test_genetic_algorithm_initialisation_2(self):
-        ga = pyeasyga.GeneticAlgorithm(
-            self.seed_data, population_size=200, generations=500,
-            elitism=False, crossover_probability=0.9,
-            mutation_probability=0.05)
-
-        fruit, profit = ga.seed_data[1]
-
-        assert len(ga.seed_data) == 4
-        assert (fruit, profit) == ('banana', 10)
-        assert ga.population_size == 200
-        assert ga.generations == 500
-        assert ga.elitism is False
-        assert ga.crossover_probability == 0.9
-        assert ga.mutation_probability == 0.05
-
-    def test_fitness_function(self):
-        ga = pyeasyga.GeneticAlgorithm(self.seed_data)
-        ga.fitness_function = lambda member: sum(
+        self.ga = pyeasyga.GeneticAlgorithm(self.seed_data)
+        self.ga.create_individual = lambda data: [
+            random.randint(0, 1) for _ in xrange(len(data))]
+        self.ga.fitness_function = lambda member, data: sum(
             [profit for (selected, (fruit, profit)) in
-             zip(member, self.seed_data) if selected and
-             len(filter(lambda x: x == 1, member)) == 2])
+             zip(member, data) if selected and
+             len(filter(lambda x: x == 1, member)) == 3])
 
-        assert ga.fitness_function(self.population[0]) == 15
-        assert ga.fitness_function(self.population[1]) == 25
-        assert ga.fitness_function(self.population[2]) == 0
-        assert ga.fitness_function(self.population[3]) == 0
-
-    def test_crossover_function(self):
-        pass
-
-    def test_mutate_function(self):
-        ga = pyeasyga.GeneticAlgorithm(self.seed_data)
+        def crossover(parent_1, parent_2):
+            index = random.randrange(1, len(parent_1))
+            child_1 = parent_1[:index] + parent_2[index:]
+            child_2 = parent_2[:index] + parent_1[index:]
+            return child_1, child_2
+        self.ga.crossover_function = crossover
 
         def mutate(individual):
             mutate_index = random.randrange(len(individual))
             individual[mutate_index] = (0, 1)[individual[mutate_index] == 0]
+        self.ga.mutate_function = mutate
 
-        ga.mutate_function = mutate
+        def tournament_selection(population):
+            size = int(len(population) / 10.0)
+            if (size == 0):
+                return random.choice(population)
+            members = random.sample(population, size)
+            max_fit = self.ga.maximise_fitness
+            members.sort(key=lambda x: x.fitness, reverse=max_fit)
+            return members[0]
+        self.ga.selection_function = tournament_selection
 
-        individual = [0, 1, 1, 0]
-        ga.mutate_function(individual)
+    def test_genetic_algorithm_initialisation_1(self):
+        ''' Test default initialisation '''
+        ga_1 = pyeasyga.GeneticAlgorithm(self.seed_data)
 
-        res = [x == y for (x, y) in zip(individual, [0, 1, 1, 0])]
+        assert ga_1.population_size == 100
+        assert ga_1.generations == 300
+        assert ga_1.elitism is True
+        assert ga_1.crossover_probability == 0.8
+        assert ga_1.mutation_probability == 0.2
 
-        assert individual != [0, 1, 1, 0]
+    def test_genetic_algorithm_initialisation_2(self):
+        ''' Test initialisation with specific values '''
+        ga_2 = pyeasyga.GeneticAlgorithm(
+            self.seed_data, population_size=200, generations=500,
+            crossover_probability=0.9, mutation_probability=0.05)
+        ga_2.elitism = False
+        ga_2.maximise_fitness = False
+        fruit, profit = ga_2.seed_data[1]
+
+        assert len(ga_2.seed_data) == 5
+        assert (fruit, profit) == ('banana', 10)
+        assert ga_2.population_size == 200
+        assert ga_2.generations == 500
+        assert ga_2.elitism is False
+        assert ga_2.maximise_fitness is False
+        assert ga_2.crossover_probability == 0.9
+        assert ga_2.mutation_probability == 0.05
+
+    def test_chromosome_initialisation_1(self):
+        chromosome = pyeasyga.Chromosome(['a', 'b', 'c'])
+
+        assert chromosome.genes == ['a', 'b', 'c']
+        assert chromosome.fitness == 0
+        assert str(chromosome) == "(0, ['a', 'b', 'c'])"
+
+    def test_chromosome_initialisation_2(self):
+        chromosome = pyeasyga.Chromosome(['d', 'e', 'f'])
+        chromosome.fitness = 20
+
+        assert chromosome.genes == ['d', 'e', 'f']
+        assert chromosome.fitness == 20
+        assert str(chromosome) == "(20, ['d', 'e', 'f'])"
+
+    def test_fitness_function(self):
+        func = self.ga.fitness_function
+        data = self.ga.seed_data
+
+        assert func(self.population[0], data) == 23
+        assert func(self.population[1], data) == 33
+        assert func(self.population[2], data) == 0
+        assert func(self.population[3], data) == 0
+
+    def test_crossover_function(self):
+        parent_1 = [1, 1, 1, 1, 1]
+        parent_2 = [0, 0, 0, 0, 0]
+        child_1, child_2 = self.ga.crossover_function(parent_1, parent_2)
+
+        assert child_1 not in [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1]]
+        assert child_2 not in [[0, 0, 0, 0, 0], [1, 1, 1, 1, 1]]
+
+    def test_mutate_function(self):
+        ''' Check that the individual is not the same after mutation
+            Check that only one bit in the individual is changed '''
+        individual = [0, 1, 1, 0, 1]
+        self.ga.mutate_function(individual)
+
+        res = [x == y for (x, y) in zip(individual, [0, 1, 1, 0, 1])]
+
+        assert individual != [0, 1, 1, 0, 1]
         assert res.count(False) == 1
 
-    def test_selection_function(self):
-        ga = pyeasyga.GeneticAlgorithm(self.seed_data)
-        ga.selection_function = lambda population: population[
-            random.randrange(len(population))]
-        individual = ga.selection_function(self.population)
+    def test_selection_function_1(self):
+        ''' Test tournament selection '''
+        individual = self.ga.selection_function(self.population)
 
-        assert len(individual) == 4
+        assert len(individual) == 5
+        assert individual in self.population
+
+    def test_selection_function_2(self):
+        ''' Test random selection '''
+        random_selection = lambda population: random.choice(population)
+        individual = random_selection(self.population)
+
+        assert len(individual) == 5
         assert individual in self.population
 
     def test_create_individual(self):
-        ga = pyeasyga.GeneticAlgorithm(self.seed_data)
-        ga.create_individual = lambda seed_data: [
-            random.randint(0, 1) for _ in seed_data]
+        data = self.ga.seed_data
+        individual = self.ga.create_individual(data)
 
-        individual = ga.create_individual(self.seed_data)
-        assert len(individual) == 4
+        assert len(individual) == 5
         assert all([ind in (0, 1) for ind in individual if ind in (0, 1)])
 
     def test_create_initial_population(self):
-        pass
+        pop_size = self.ga.population_size
+        func = self.ga.create_individual
+        data = self.ga.seed_data
+
+        initial_population = self.ga.create_initial_population(
+            data, pop_size, func)
+
+        assert len(initial_population) == pop_size
+        assert isinstance(initial_population[0],
+                          type(pyeasyga.Chromosome([1])))
+        assert sum([member.fitness for member in initial_population]) == 0
 
     def test_calculate_population_fitness(self):
-        pass
+        data = self.ga.seed_data
+        pop_size = self.ga.population_size
+        create_func = self.ga.create_individual
+
+        population = self.ga.create_initial_population(
+            data, pop_size, create_func)
+
+        fitness_func = self.ga.fitness_function
+        self.ga.calculate_population_fitness(data, population, fitness_func)
+
+        assert sum([member.fitness for member in population]) > 0
 
     def test_rank_population(self):
-        pass
+        data = self.seed_data
+
+        pop_size = self.ga.population_size
+        create_func = self.ga.create_individual
+        fitness_func = self.ga.fitness_function
+
+        population = self.ga.create_initial_population(
+            data, pop_size, create_func)
+        self.ga.calculate_population_fitness(data, population, fitness_func)
+
+        new_population = copy.deepcopy(population)
+        new_population.sort(key=attrgetter('fitness'), reverse=True)
+
+        self.ga.rank_population(population, True)
+
+        assert population[0].fitness == new_population[0].fitness
+        assert population[50].fitness == new_population[50].fitness
+        assert population[99].fitness == new_population[99].fitness
 
     def test_create_new_population(self):
-        pass
+        data = self.seed_data
+
+        pop_size = self.ga.population_size
+        create_func = self.ga.create_individual
+        fitness_func = self.ga.fitness_function
+
+        population = self.ga.create_initial_population(
+            data, pop_size, create_func)
+        self.ga.calculate_population_fitness(data, population, fitness_func)
+        self.ga.rank_population(population, True)
+
+        crossover = self.ga.crossover_function
+        prob_crossover = self.ga.crossover_probability
+        mutate = self.ga.mutate_function
+        prob_mutate = self.ga.mutation_probability
+        selection = self.ga.selection_function
+        elitism = True
+
+        new_population = self.ga.create_new_population(
+            population, crossover, prob_crossover, mutate, prob_mutate,
+            selection, elitism)
+
+        assert len(new_population) == self.ga.population_size
+        assert new_population[0].fitness == population[0].fitness
+        assert new_population[0].genes == population[0].genes
 
     def test_create_first_generation(self):
-        pass
+        data = self.ga.seed_data
+        pop_size = self.ga.population_size
+        create_func = self.ga.create_individual
+        fitness_func = self.ga.fitness_function
+        max_fitness = self.ga.maximise_fitness
+
+        population = self.ga.create_first_generation(
+            data, pop_size, create_func, fitness_func, max_fitness)
+
+        assert len(population) == pop_size
+        assert isinstance(population[0], type(pyeasyga.Chromosome([1])))
+        ''' see if can add better test '''
 
     def test_create_next_generation(self):
-        pass
+        data = self.ga.seed_data
+        pop_size = self.ga.population_size
+        create_func = self.ga.create_individual
+        fitness_func = self.ga.fitness_function
+        selection = self.ga.selection_function
+        crossover = self.ga.crossover_function
+        prob_crossover = self.ga.crossover_probability
+        mutate = self.ga.mutate_function
+        prob_mutate = self.ga.mutation_probability
+        elitism = self.ga.elitism
+        max_fitness = self.ga.maximise_fitness
+
+        population = self.ga.create_first_generation(
+            data, pop_size, create_func, fitness_func, max_fitness)
+
+        next_gen = self.ga.create_next_generation(
+            data, population, fitness_func, selection, crossover,
+            prob_crossover, mutate, prob_mutate, elitism, max_fitness)
+
+        assert len(next_gen) == pop_size
+        assert isinstance(population[0], type(pyeasyga.Chromosome([1])))
+        ''' see if can add better test '''
+
+    def test_run(self):
+        self.ga.run()
+        current_gen = self.ga.current_generation
+        last_generation = self.ga.last_generation()
+
+        assert len(current_gen) == self.ga.population_size
+        assert isinstance(current_gen[0], type(pyeasyga.Chromosome([1])))
+        assert isinstance(last_generation.next(), type((1, [1, 1, 1, 1, 1])))
+        assert len(last_generation.next()) == 2
+        assert len(last_generation.next()[1]) == 5
+        ''' see if can add better test '''
 
     def test_best_individual(self):
-        pass
+        data = self.seed_data
+
+        pop_size = self.ga.population_size
+        create_func = self.ga.create_individual
+        fitness_func = self.ga.fitness_function
+
+        population = self.ga.create_initial_population(
+            data, pop_size, create_func)
+        self.ga.calculate_population_fitness(data, population, fitness_func)
+        self.ga.rank_population(population, True)
+
+        best_fitness, best_genes = self.ga.best_individual(population)
+
+        assert best_fitness == population[0].fitness
+        assert best_genes == population[0].genes
 
     def test_last_generation(self):
-        pass
+        pop_size = self.ga.population_size
+        create_func = self.ga.create_individual
+        fitness_func = self.ga.fitness_function
+        data = self.ga.seed_data
+        max_fitness = self.ga.maximise_fitness
+
+        self.ga.current_generation = self.ga.create_first_generation(
+            data, pop_size, create_func, fitness_func, max_fitness)
+        last_generation = self.ga.last_generation()
+
+        assert isinstance(last_generation.next(), type((1, [1, 1, 1, 1, 1])))
+        assert len(last_generation.next()) == 2
+        assert len(last_generation.next()[1]) == 5
+        ''' see if can add better test '''
 
     def tearDown(self):
         pass
