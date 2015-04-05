12090c6ef99f5aa08e02151229cafcd9e40b676a
Yasser Gonzalez <yasserglez@gmail.com>
2015-04-05 02:12:33 -0400

    List Python 3.4 in setup.py

--- setup.py
+++ setup.py
@@ -60,6 +60,8 @@ setup(
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
     test_suite='tests',
